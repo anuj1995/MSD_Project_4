686d3ceccc0ed318003b2edda63965e7c94d82ad
sdvickers98@gmail.com <sdvickers98@gmail.com>
2020-04-12 01:38:35 -0400

    changed quiztablerow

--- app/src/main/java/edu/uga/cs/quiz_app/ui/gallery/QuizHistoryTableRow.java
+++ app/src/main/java/edu/uga/cs/quiz_app/ui/gallery/QuizHistoryTableRow.java
@@ -1,6 +1,7 @@
 package edu.uga.cs.quiz_app.ui.gallery;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -13,19 +14,19 @@ public class QuizHistoryTableRow extends TableRow {
         TextView userView = new TextView(context);
         userView.setLayoutParams(new TableLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 1f));
         userView.setText(user);
-        userView.setTextColor(0);
+        userView.setTextColor(Color.BLACK);
         userView.setTextSize(22);
 
         TextView dateView = new TextView(context);
         dateView.setLayoutParams(new TableLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 1f));
         dateView.setText(date);
-        dateView.setTextColor(0);
+        dateView.setTextColor(Color.BLACK);
         dateView.setTextSize(22);
 
         TextView scoreView = new TextView(context);
         scoreView.setLayoutParams(new TableLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 1f));
         scoreView.setText(score);
-        scoreView.setTextColor(0);
+        scoreView.setTextColor(Color.BLACK);
         scoreView.setTextSize(22);
 
         addView(userView);
