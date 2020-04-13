f14354c603f353ac5b2b0bc3f504c0bf0a792368
anuj panchmia <ap83035@uga.edu>
2020-04-12 23:40:33 -0400

    added a toast

--- app/src/main/java/edu/uga/cs/quiz_app/ui/quiz/QuizSubmitFragment.java
+++ app/src/main/java/edu/uga/cs/quiz_app/ui/quiz/QuizSubmitFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
@@ -13,16 +14,7 @@ import edu.uga.cs.quiz_app.R;
 
 public class QuizSubmitFragment extends Fragment {
 
-    public static QuizSubmitFragment newInstance() {
-
-        final QuizSubmitFragment mf = new QuizSubmitFragment();
-
-        return mf;
-    }
-
-    public QuizSubmitFragment() {
-    }
-
+    ScoreViewModel scoreViewModel;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,17 +24,31 @@ public class QuizSubmitFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate and locate the main ImageView
+        scoreViewModel =ScoreViewModel.getInstance();
         final View v = inflater.inflate(R.layout.fragment_quiz_submit, container, false);
 
         Button submit = v.findViewById(R.id.submit_button);
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), QuizEndActivity.class);
-                startActivity(intent);
+
+                if(checkCompletion()){
+                    Intent intent = new Intent(getActivity(), QuizEndActivity.class);
+                    startActivity(intent);
+                }
+                else {
+                    Toast.makeText(getContext(),"you have missed some options please mark them and submit", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
 
+
         return v;
+    }
+
+    private boolean checkCompletion(){
+        return (scoreViewModel.getContinentScore().size() == 6 && scoreViewModel.getNeighbourScore().size() == 6);
+
     }
 }
